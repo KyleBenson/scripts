9a67cc4f2885c98c1cd2be826f155edaabb59e80
Kyle Benson <kebenson@uci.edu>
2013-01-15 16:29:51 -0800

    Finished up the forking example.

--- bash_examples/forking.sh
+++ bash_examples/forking.sh
@@ -1,3 +1,35 @@
 #! /bin/bash
+## Simple script to demonstrate PIDs and inheritance of environment variables when forking.
 
-function 
+function inf_loop {
+    while true
+    do
+	echo "Process $BASHPID sleeping and VARIABLE is $VARIABLE"
+	sleep 3
+    done
+}
+
+# Executed when child is forked
+function child {
+    echo "Child PID: $BASHPID, VARIABLE is $VARIABLE"
+    echo "Child changing VARIABLE to 30"
+    VARIABLE=30
+    inf_loop
+}
+
+# Executed when Ctrl-C is pressed
+function cleanup {
+    echo parent killed. stopping child
+    kill -9 $!
+    exit
+}
+
+VARIABLE=20
+
+echo "Parent PID: $BASHPID, VARIABLE is $VARIABLE"
+
+(child) &
+echo "Child process started"
+
+#trap cleanup SIGINT
+inf_loop
