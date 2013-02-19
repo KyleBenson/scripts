c276b87a4ec2d5fce2875295108d9c9821b64fb6
Kyle Benson <kebenson@uci.edu>
2013-02-19 15:18:59 -0800

    Fixed future import statement

--- new_args_script.py
+++ new_args_script.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python
+from __future__ import print_function
 NEW_SCRIPT_DESCRIPTION = '''Description that will appear when help is printed.'''
 
 # @author: Kyle Benson
 # (c) Kyle Benson 2012
 
 import argparse
-from future import print_function
 #from os.path import isdir
 #from os import listdir
 #from getpass import getpass
