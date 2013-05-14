f9b95ed24f8db4130b11c3154a1ad56cfc32d700
Kyle Benson <kyle.edward.benson@gmail.com>
2013-05-13 20:59:45 -0700

    Added script to replace a substring in each file name found in a directory, recursively, with another substring.

---
+++ python/filename_replace.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+usage = '''
+Usage: filename_replace.py from to [dir]
+Walks through the possibly specified directory ($PWD by default), dir, recursively and replaces occurrences of 'from' in the filenames with the string 'to'.
+Useful for changing file extensions.
+'''
+
+import os, sys
+
+def rename_files(from_pattern, to_pattern, path_to_walk):
+    # find every file in the current directory recursively
+    for root, dirs, files in os.walk(path_to_walk):
+        # walk through all possible numbers U1 would add to the filename
+        for f in files:
+            if from_pattern in f:
+                newname = os.path.join(root, f.replace(from_pattern, to_pattern))
+                os.rename(os.path.join(root, f), newname)
+                
+                if not os.path.exists(newname):
+                    print 'error renaming file!'
+            
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print 'Not enough arguments!\n%s' % usage
+    elif len(sys.argv) > 3:
+        rename_files(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        rename_files(sys.argv[1], sys.argv[2], os.getcwd())
