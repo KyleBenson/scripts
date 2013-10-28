5f1fabbcffd2dd90ad2927ead7eca7930e6edf8a
Kyle Benson <kyle.edward.benson@gmail.com>
2013-10-28 02:43:49 -0700

    fixed up ubuntu-upgrade and added more files

--- ubuntu-upgrade.py
+++ ubuntu-upgrade.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 from __future__ import print_function
 from shutil import copy2
+from sys import args
 
-files_to_backup = ['/etc/gnome/defaults.list',
-                   '/etc/default/rcS',
-                   ]
+if len(args) > 1:
+    files_to_backup = args[1:]
+else:
+    files_to_backup = [
+        '/etc/gnome/defaults.list',
+        '/etc/default/rcS',
+        ]
 
 for f in files_to_backup:
     print("backing up %s" % f)
