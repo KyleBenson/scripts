494a818dcee627b62052bb847ee18e5d57752fbe
Kyle Benson <kyle.edward.benson@gmail.com>
2015-05-20 13:51:55 -0700

    filename_replace.py searches/replaces directories too

--- python/filename_replace.py
+++ python/filename_replace.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 
 usage = '''
 Usage: filename_replace.py from to [dir]
-Walks through the possibly specified directory ($PWD by default), dir, recursively and replaces occurrences of 'from' in the filenames with the string 'to'.
+Walks through the possibly specified directory ($PWD by default), dir, recursively and replaces occurrences of 'from' in the filenames/dirnames with the string 'to'.
 Useful for changing file extensions.
 '''
 
@@ -13,14 +13,14 @@ def rename_files(from_pattern, to_pattern, path_to_walk):
     # find every file in the current directory recursively
     for root, dirs, files in os.walk(path_to_walk):
         # walk through all possible numbers U1 would add to the filename
-        for f in files:
+        for f in files + dirs:
             if from_pattern in f:
                 newname = os.path.join(root, f.replace(from_pattern, to_pattern))
                 os.rename(os.path.join(root, f), newname)
-                
+
                 if not os.path.exists(newname):
                     print('error renaming file!')
-            
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
