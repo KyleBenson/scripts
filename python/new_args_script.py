d0c1301df68c68e4de3d98501a6cbc046a88d43b
Kyle Benson <kyle.edward.benson@gmail.com>
2017-03-12 13:01:23 -0700

    some notes about python args scripts

--- python/new_args_script.py
+++ python/new_args_script.py
@@ -19,11 +19,15 @@ def parse_args(args):
 # action is one of: store[_const,_true,_false], append[_const], count
 # nargs is one of: N, ?(defaults to const when no args), *, +, argparse.REMAINDER
 # help supports %(var)s: help='default value is %(default)s'
+# Mutually exclusive arguments:
+# group = parser.add_mutually_exclusive_group()
+# group.add_argument(...)
 ##################################################################################
 
     parser = argparse.ArgumentParser(description=NEW_SCRIPT_DESCRIPTION,
                                      #formatter_class=argparse.RawTextHelpFormatter,
                                      #epilog='Text to display at the end of the help print',
+                                     #parents=[parent1,...], # add parser args from these ArgumentParsers
                                      )
 
     parser.add_argument('--files', '-f', type=str, nargs='+',
@@ -35,6 +39,14 @@ def parse_args(args):
 
 
     return parser.parse_args(args)
+
+# NOTE: using argparse with class constructors can be tricky.  While you may be
+# tempted to simply 'unzip' the vars(args) dict into the __init__ args, this
+# can cause some difficult situations.  For one, you sometimes need to do more
+# than simply store self.arg1 = arg1 (e.g. set logging level from debug).
+# You also will run into more issues with using the 'parents' option as one
+# constructor won't be able to understand args introduced by the other(s).
+# Lastly, this unzipping approach doesn't well-document the class attributes.
 
 # Main
 if __name__ == "__main__":
